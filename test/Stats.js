52067622d233999d21d530776094794517b5280f
Artur Cistov <artchistov@gmail.com>
2013-12-21 01:24:21 +0000

    New data structure

--- test/Stats.js
+++ test/Stats.js
@@ -1,15 +1,50 @@
 var fs = require('fs'),
     should   = require('should'),
     Stats = require('../lib/Stats'),
-    sampleData = require('./fixtures/parsed');
+    sampleData = require('./fixtures/parsed'),
+    stats = Stats.generate(sampleData);
+
+/*
+Expected format:
+
+[
+     {
+      "from": { "name": "Leytonstone", "type": "underground" },
+      "to": { "name": "Stratford", "type": "underground" },
+      "times": {
+        "data": [19, 20, 21, 18],
+        "stats": {
+              "size": 4,
+              "min": 18,
+              "max": 21,
+              "mean": 19.5,
+              "median": 19.5,
+              "stdev": 1.29
+            }
+      }
+     }
+   ]
+*/
 
 describe('Stats', function() {
 
-  describe('Reports', function() {
+  describe('Report data structure', function() {
 
-    it('Generation', function() {
-        var stats = Stats.generate(sampleData);
-        console.log(JSON.stringify(stats));
+    it('Journeys', function() {
+        stats.should.be.an.instanceOf(Array);
+    });
+
+    it('Journey', function() {
+      var journey = stats[0];
+      journey.should.have.property('to');
+      journey.should.have.property('from');
+      journey.should.have.property('times');
+    });
+
+    it('Journey times', function() {
+      var journey = stats[0];
+      journey.times.should.have.property('data');
+      journey.times.should.have.property('stats');
     });
 
   });
